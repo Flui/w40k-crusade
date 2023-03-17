7924c0dc16433492d38217fc3248e9079054c529
Florian Schultze <f.schultze@mailbox.org>
2023-03-17 10:15:43 +0100

    chore: add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+
 import "./App.css";
 
+const theme = createTheme();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
