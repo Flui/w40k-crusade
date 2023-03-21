7b514037d6ddb7284f5c6c521d851629c14ec111
Florian Schultze <f.schultze@mailbox.org>
2023-03-21 23:47:14 +0100

    feat: change primary color

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,10 @@
 import { createTheme } from "@mui/material/styles";
 
 // A custom theme for this app
-const theme = createTheme();
+const theme = createTheme({
+  palette: {
+    primary: { main: "#2478a0" },
+  },
+});
 
 export default theme;
